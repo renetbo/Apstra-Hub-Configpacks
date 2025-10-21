d2a4dd52f5c4a8a5c2994305f300d6cef92bdeea
Boris RENET <brenet@juniper.net>
2025-10-21 19:05:02 +0200

    init

--- pack/tofu/terraform.tf
+++ pack/tofu/terraform.tf
@@ -6,25 +6,7 @@ terraform {
   required_providers {
     apstra = {
       source = "Juniper/apstra"
-      version = "0.92.2"
+      version = "~> 0.92.2"
     }
   }
-}
-
-provider "apstra" {
-  # URL and credentials can be supplied using the "url" parameter in this file.
-  # 4.2.1
-  # url = "https://admin:Juniper123!@10.85.44.65:11343" 
-  # 5.1.0  
-  # url = "https://admin:Poclab123!@172.30.193.12"
-  # 6.0.0
-    url = "https://admin:Poclab123!@172.30.193.23"
-  #
-  # ...or using the environment variable APSTRA_URL.
-  #
-  # If Username or Password are not embedded in the URL, the provider will look
-  # for them in the APSTRA_USER and APSTRA_PASS environment variables.
-  #
-  tls_validation_disabled = true  # CloudLabs doesn't present a valid TLS cert
-  blueprint_mutex_enabled = false # Don't attempt worry about competing clients
 }
